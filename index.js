ef5ca8cdecc44c5f25a1fe5ab2da4eb619d6de63
Dabit <dabit@8c85901b6e1c.ant.amazon.com>
2019-06-25 02:31:18 -0700

    minor updates

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
+window.LOG_LEVEL = 'DEBUG'
+
 import config from './aws-exports'
 import Amplify from 'aws-amplify'
 Amplify.configure(config)
