9fbc24078419d45279d9c015b1daa07e73066d2d
Dabit <dabit@8c85901b6e1c.ant.amazon.com>
2019-06-25 02:17:18 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import App from './App';
+import {name as appName} from './app.json';
+
+import config from './aws-exports'
+import Amplify from 'aws-amplify'
+Amplify.configure(config)
+
+AppRegistry.registerComponent(appName, () => App);
