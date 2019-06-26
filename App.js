228d92700e93676c5fb87e36e95842a084063967
Dabit <dabit@8c85901b6e1c.ant.amazon.com>
2019-06-26 06:50:37 -0700

    forgot to merge hooks version

--- App.js
+++ App.js
@@ -1,141 +1,141 @@
-import React, {Component} from 'react';
+import React, {useReducer, useEffect} from 'react';
 import {Button, TextInput, StyleSheet, Text, View} from 'react-native';
 import { Auth } from 'aws-amplify'
 
 const INPUT = { height: 35, width: 200, backgroundColor: '#ddd', marginBottom: 10 }
+const initialState = {
+  loading: true, confirmationCode: '', username: '', password: '', email: '', formState: 'signUp'
+}
 
-class App extends Component {
-  state = {
-    loading: true, confirmationCode: '', username: '', password: '', email: '', formState: 'signUp'
+function reducer(state, action) {
+  switch(action.type) {
+    case 'SET_INPUT':
+      return { ...state, [action.name]: action.value}
+    case 'SET_FORM_STATE':
+      return { ...state, loading: false, formState: action.formState }
+    default:
+      return state
   }
-  componentDidMount() {
+}
+function App() {
+  const [state, dispatch] = useReducer(reducer, initialState)
+  useEffect(() => {
     Auth.currentAuthenticatedUser()
-      .then(userInfo => {
-        this.setState({ formState: 'signedIn', loading: false })
-      })
-      .catch(err => {
-        this.setState({ formState: 'signUp', loading: false })
-      });
+      .then(userInfo => dispatch({ type: 'SET_FORM_STATE', formState: 'signedIn' }))
+      .catch(err => dispatch({ type: 'SET_FORM_STATE', formState: 'signUp' }))
+  }, [])
+  function onChange (name, value) {
+    dispatch({ type: 'SET_INPUT', name, value })
   }
-  onChange = (key, value) => { this.setState({ [key]: value })}
-  signUp = () => {
-    const { username,password, email } = this.state
-    Auth.signUp({
-      username,
-      password,
-      attributes: {
-        email
-      }
-      })
+  function signUp() {
+    const { username,password, email } = state
+    Auth.signUp({ username, password, attributes: { email } })
       .then(data => {
-        console.log("Success: ", data)
-        this.setState({ formState: 'confirmSignUp' })
+        dispatch({ type: 'SET_FORM_STATE', formState: 'confirmSignUp' })
       })
       .catch(err => console.log(err));
   }
-  confirmSignUp = () => {
-    const { username, confirmationCode } = this.state
+  function confirmSignUp() {
+    const { username, confirmationCode } = state
     Auth.confirmSignUp(username, confirmationCode)
       .then(data => {
         console.log('successfully confirmed sign up')
-        this.setState({ formState: 'signIn' })
+        dispatch({ type: 'SET_FORM_STATE', formState: 'signIn' })
       })
       .catch(err => console.log(err));
   }
-  signIn = () => {
-    const { username, password } = this.state
+  function signIn()  {
+    const { username, password } = state
     Auth.signIn(username, password)
       .then(user => {
         console.log('user: ', user)
-        this.setState({ formState: 'signedIn' })
+        dispatch({ type: 'SET_FORM_STATE', formState: 'signedIn' })
       })
       .catch(err => console.log(err));
   }
   showSignIn = () => {
-    this.setState({ formState: 'signIn' })
+    dispatch({ type: 'SET_FORM_STATE', formState: 'signIn' })
   }
-  render() {
-    const { formState, loading } = this.state
-    if (loading) return <Text>Loading...</Text>
-    return (
-      <View style={styles.container}>
-        {
-           formState === 'signedIn' && (
-            <Text style={{marginTop: 100}}>Hello World</Text>
-           )
-        }
-        {
-          formState === 'signIn' && (
-            <View>
-              <Text>Hello from sign in</Text>
-              <TextInput
-                style={INPUT}
-                placeholder='username'
-                onChangeText={val => this.onChange('username', val)}
-              />
-              <TextInput
-                secureTextEntry={true}
-                style={INPUT}
-                placeholder='password'
-                onChangeText={val => this.onChange('password', val)}
-              />
-              <Button
-                title="Sign In" onPress={this.signIn}
-              />
-            </View>
-          )
-        }
-        {
-          formState === 'signUp' && (
-            <View>
-              <TextInput
-                style={INPUT}
-                placeholder='username'
-                onChangeText={val => this.onChange('username', val)}
-              />
-              <TextInput
-                style={INPUT}
-                placeholder='email'
-                onChangeText={val => this.onChange('email', val)}
-              />
-              <TextInput
-                secureTextEntry={true}
-                style={INPUT}
-                placeholder='password'
-                onChangeText={val => this.onChange('password', val)}
-              />
-              <Button
-                title="Sign Up" onPress={this.signUp}
-              />
-            </View>
-          )
-        }
-        {
-          formState === 'confirmSignUp' && (
-            <View>
-              <TextInput
-                style={INPUT}
-                placeholder='confirmationCode'
-                onChangeText={val => this.onChange('confirmationCode', val)}
-              />
-              <Button
-                title="Confirm Sign Up" onPress={this.confirmSignUp}
-              />
-            </View>
-          )
-        }
-        <Button
-          title="Show Sign In" onPress={this.showSignIn}
-        />
-        <Button
-          title="Sign Out" onPress={() => {
-            Auth.signOut()
-            this.setState({ formState: 'signIn' })
-          }}
-        />
-      </View>
-    );
-  }
+  const { formState, loading } = state
+  if (loading) return <Text>Loading...</Text>
+  return (
+    <View style={styles.container}>
+      {
+         formState === 'signedIn' && (
+          <Text style={{marginTop: 100}}>Hello World</Text>
+         )
+      }
+      {
+        formState === 'signIn' && (
+          <View>
+            <Text>Hello from sign in</Text>
+            <TextInput
+              style={INPUT}
+              placeholder='username'
+              onChangeText={val => onChange('username', val)}
+            />
+            <TextInput
+              secureTextEntry={true}
+              style={INPUT}
+              placeholder='password'
+              onChangeText={val => onChange('password', val)}
+            />
+            <Button
+              title="Sign In" onPress={signIn}
+            />
+          </View>
+        )
+      }
+      {
+        formState === 'signUp' && (
+          <View>
+            <TextInput
+              style={INPUT}
+              placeholder='username'
+              onChangeText={val => onChange('username', val)}
+            />
+            <TextInput
+              style={INPUT}
+              placeholder='email'
+              onChangeText={val => onChange('email', val)}
+            />
+            <TextInput
+              secureTextEntry={true}
+              style={INPUT}
+              placeholder='password'
+              onChangeText={val => onChange('password', val)}
+            />
+            <Button
+              title="Sign Up" onPress={signUp}
+            />
+          </View>
+        )
+      }
+      {
+        formState === 'confirmSignUp' && (
+          <View>
+            <TextInput
+              style={INPUT}
+              placeholder='confirmationCode'
+              onChangeText={val => onChange('confirmationCode', val)}
+            />
+            <Button
+              title="Confirm Sign Up" onPress={confirmSignUp}
+            />
+          </View>
+        )
+      }
+      <Button
+        title="Show Sign In" onPress={showSignIn}
+      />
+      <Button
+        title="Sign Out" onPress={() => {
+          Auth.signOut()
+          setState({ formState: 'signIn' })
+        }}
+      />
+    </View>
+  );
 }
 
 export default App
